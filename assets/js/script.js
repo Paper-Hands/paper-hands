6c785a957332da7000a8e657af9916d1e5dcdfce
brandon hicks <brandonhicks751@gmail.com>
2022-11-07 21:31:49 -0600

    display stock info on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,21 @@ const API_KEY = '2RKX3B5PK69BTLCH';
 const baseUrl = 'https://www.alphavantage.co/query?';
 let queryFunction = 'TIME_SERIES_INTRADAY';
 let interval = '15min';
-let activeStock = $('#listed-stock');
-
+// var activeStock = $('.display-stock');
+var activeStock = document.getElementById('display-stock')
+let searchField = $('.user-input');
 /** 
  * Makes a fetch request and returns the stock data with the specified parameters in JSON
  * @param url - The url to send the web request to
  * @param func - The time series to use
  * @param interval - the time interval between data points. Default value is 5 if not given
 */
- const getStock = (url, func, sym, interv=5) => {
+ function getStock(url, func, sym, interv=5) {
 
     // const testApiReqUrl = `${url}function=${func}&symbol=${sym}&interval=${interv}min&apikey=${API_KEY}`;
     let responseData = [];
     let request = `${url}function=${func}&symbol=${sym}&interval=${interv}min&apikey=${API_KEY}`;
-
+    let res = [];
 
     console.log(request);
 
@@ -24,17 +25,26 @@ let activeStock = $('#listed-stock');
         return response.json();
       })
     .then(function(data){
+        console.log(data);
         let symbol = data['Meta Data']['2. Symbol'];
         var lastRefreshed = data['Meta Data']['3. Last Refreshed'];
         var lastTradePriceOnly = data['Time Series (5min)'][lastRefreshed]['4. close'];
         var lastVolume = data['Time Series (5min)'][lastRefreshed]['5. volume'];
-
-    console.log(symbol + ', ' + lastRefreshed + ', ' + lastTradePriceOnly + ', ' + lastVolume);
-    activeStock.html(symbol + ', ' + lastRefreshed + ', ' + lastTradePriceOnly + ', ' + lastVolume);
+        
+     console.log(symbol + ', ' + lastRefreshed + ', ' + lastTradePriceOnly + ', ' + lastVolume);
+    console.log(activeStock)
+    let temp = `${symbol}, ${lastRefreshed}, ${lastTradePriceOnly}, ${lastVolume}`
+    console.log(temp)
+    // activeStock.appendChild(temp)
+    activeStock.textContent = temp
     })
-    return;
+}
+// Capture user input from input forms 
+const getUserInput = () => {
+
 }
  
+console.log(`STOCK DATA : ${getStock(baseUrl, queryFunction, 'IBM', 5)}`);
 
  //function() {}
 
