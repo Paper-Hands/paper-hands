0fce06bea8793d944944a4dc7466b955b00323ca
brandon hicks <brandonhicks751@gmail.com>
2022-11-08 19:47:45 -0600

    cleaned up code and added basic functionality to getUserInput function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ const API_KEY = '2RKX3B5PK69BTLCH';
 const baseUrl = 'https://www.alphavantage.co/query?';
 let queryFunction = 'TIME_SERIES_INTRADAY';
 let interval = '15min';
-// var activeStock = $('.display-stock');
 var activeStock = document.getElementById('display-stock')
-let searchField = $('.user-input');
+const searchField = $('#search-bar');
+const searchButton = $('#search-btn');
+
 /** 
  * Makes a fetch request and returns the stock data with the specified parameters in JSON
  * @param url - The url to send the web request to
@@ -12,9 +13,6 @@ let searchField = $('.user-input');
  * @param interval - the time interval between data points. Default value is 5 if not given
 */
 function getStock(url, func, sym, interv = 5) {
-
-  // const testApiReqUrl = `${url}function=${func}&symbol=${sym}&interval=${interv}min&apikey=${API_KEY}`;
-  // let responseData = [];
   let request = `${url}function=${func}&symbol=${sym}&interval=${interv}min&apikey=${API_KEY}`; // request url
   console.log(request); // log out the request url
 
@@ -31,58 +29,30 @@ function getStock(url, func, sym, interv = 5) {
       var lastTradePriceOnly = data['Time Series (5min)'][lastRefreshed]['4. close'];
       var lastVolume = data['Time Series (5min)'][lastRefreshed]['5. volume'];
 
-      // console.log(symbol + ', ' + lastRefreshed + ', ' + lastTradePriceOnly + ', ' + lastVolume);
       console.log(activeStock)
       let temp = `${symbol}, ${lastRefreshed}, ${lastTradePriceOnly}, ${lastVolume}`
       console.log(temp)
-      // activeStock.appendChild(temp)
       activeStock.textContent = temp
     })
 }
 // Capture user input from input forms 
 const getUserInput = () => {
-  
-}
+  const radioButtons = $('input[name="group1"]');
+  console.log($("input[type='radio'][name='group1']:checked").val());
+  let symbol = searchField.val();
+  let interval;
+  console.log(`SYM: ${symbol}`);
 
+}
 
 // event handler for search button
 const searchButtonHandler = (e) => {
-
-
+  getUserInput();
 }
 
+searchButton.on('click', searchButtonHandler);
+
 console.log(`STOCK DATA : ${getStock(baseUrl, queryFunction, 'IBM', 5)}`);
-
- //function() {}
-
-//let testStock = getStock(baseUrl, queryFunction, 'IBM', 5);
-//console.log(testStock);
-//console.log(getStock(baseUrl, queryFunction, 'IBM', 5));
-
-// class StockData {
-//     symbol; // symbol of the stock
-//     timeSeries; // time series 
-//     constructor() {
-
-//     }
-
-//     printData() {
-
-//     }
-// }
-
-// class CryptoData {
-//     symbol; // symbol of cryptocurrency; for example symbol=BTC
-//     timeSeries; // time series; for example timeSeries = CRYPTO_INTRADAY
-//     market; // market to convert crypto to; for example market=USD
-
-//     constructor() {
-
-//     }
-//     printData() {
-
-//     }
-// }
 
 //button test
 ﻿
