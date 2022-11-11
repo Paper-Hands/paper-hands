7b33e7a8f3783b7548d01184ef116649ed90496e
Moses C <113498167+MoZCa@users.noreply.github.com>
2022-11-10 21:50:05 -0600

    added time

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,11 @@ var displayTickerEl = $('#display-ticker');
 var lastTradeEl = $('#last-trade');
 var sharesTradedEl = $('#shares-traded');
 
+var newYork = moment.tz("America/New_York").format('lll');
+console.log(newYork);
+
+var time = $(".time").html(newYork);
+
 /** 
  * Makes a fetch request and returns the stock data with the specified parameters in JSON
  * @param url - The url to send the web request to
